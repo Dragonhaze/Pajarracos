8843ab9d6d0dd41ee5f3b2ac43bc82d9553e1a97
Dragonhaze <duncanporter@hotmail.es>
2018-01-08 12:05:03 +0100

    Comentarios en consultarCliente

--- Pajarracos/Pajarracos/ConsultarCliente.xaml.cs
+++ Pajarracos/Pajarracos/ConsultarCliente.xaml.cs
@@ -17,23 +17,28 @@ namespace Pajarracos
 {
     /// <summary>
     /// Lógica de interacción para ConsultarCliente.xaml
+    /// Como bien dice el nombre de la clase esta es la ventana que sirve para consultar clientes
     /// </summary>
     public partial class ConsultarCliente : Window
-    {
+    {   /// <summary>
+        /// Método para consultar clientes
+        /// </summary>
         public ConsultarCliente()
         {
 
-
+            
+            // Se inicializa la conexión de base de datos
             InitializeComponent();
             var dbCon = DBConnection.Instance();
             dbCon.DatabaseName = "pajareria";
 
             if (dbCon.IsConnect())
             {
+                //Esta es la query
                 string query = "SELECT * FROM Clientes;";
                 var cmd = new MySqlCommand(query, dbCon.Connection);
                 MySqlDataReader rdr = cmd.ExecuteReader();
-
+                //Con el reader imprime 
                 while (rdr.Read())
                 {
                     texto.Text += "Id del cliente : " + rdr[0].ToString() + '\n' +
