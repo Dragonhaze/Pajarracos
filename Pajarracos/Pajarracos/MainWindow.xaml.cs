99327ab566505aba36e70da0ef984c4e1d314ef0
Dragonhaze <duncanporter@hotmail.es>
2018-02-05 12:22:08 +0100

    Insertada ventana para ver el report

--- Pajarracos/Pajarracos/MainWindow.xaml.cs
+++ Pajarracos/Pajarracos/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Pajarracos
 {
@@ -64,6 +51,12 @@ namespace Pajarracos
 
                     window4.ShowDialog();
                     break;
+                case 4:
+                    Form1 form1 = new Form1();
+
+                    form1.ShowDialog();
+                    break;
+
 
             }
         }
